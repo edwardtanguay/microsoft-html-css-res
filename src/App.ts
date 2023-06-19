da7c0306403008d000c18b331cec2042be184124
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-19 22:26:54 +0200

    page in

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,28 @@
 import './styles/main.scss';
+import './styles/responsive.scss';
 import { getMenu, getCurrentPage } from './Router';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
 <header>
-	<h1>Info Site</h1>
+    <img width="116" src="images/logo.png" alt="" />
 	${getMenu()}
 </header>
 <main>
 	${getCurrentPage()}
 </main>
+<footer>
+	<label for="pet-select">English (United States)</label>
+	<select name="pets" id="pet-select" >Languch
+	<option value="english">English</option>
+	<option value="deutsch">Deutsch</option>
+	</select>
+	<a href="#">Sitemap</a>
+	<a href="#">Contact Microsoft</a>
+	<a href="#">privacy & cookies</a>
+	<a href="#">Terms of use</a>
+	<a href="#">Trademarks</a>
+	<a href="#">Safety & eco</a>
+	<a href="#">About our ads</a>
+	<a href="#">© Microsoft 2023</a>
+</footer>
 `;
--- src/pages/PageOffice.ts
+++ src/pages/PageOffice.ts
@@ -1,7 +1,175 @@
 export const PageOffice = () => {
 	return /*html*/`
 <div class="page pageOffice">
-	<p>This is the office page.</p>
+    <section class="hero">
+      <h2>Surface Deals</h2>
+      <p>Select Surface are on sale now - save while supplies last</p>
+      <button><a href="#">Shop Now</a></button>
+    </section>
+
+    <div class="card-container">
+      <div class="card">
+        <img src="images/card1.png" alt="" />
+        <h3>New Surface Pro.</h3>
+        <p>
+          See how Katie Sowers, Asset. Coach for the 49ers,uses Surface Pro 7 to
+          put her plans into play.
+        </p>
+        <a href="#">LEARN MORE</a>
+      </div>
+
+      <div class="card">
+        <img src="images/card2.png" alt="" />
+        <h3>New Surface Laptop 3</h3>
+        <p>
+          Express Yourself Powerfully with a thin, light, and elegant design,
+          faster performance, and up to 11.5 hours battery life.
+        </p>
+        <a href="#">LEARN MORE</a>
+      </div>
+
+      <div class="card">
+        <img src="images/card3.png" alt="" />
+        <h3>Save $150 + free controller</h3>
+        <p>
+          Buy an Xbox One X console and double your fun with a free select extra
+          controller. Starting at $349
+        </p>
+        <a href="#">LEARN MORE</a>
+      </div>
+
+      <div class="card">
+        <img src="images/card4.png" alt="" />
+        <h3>The new Microsoft Edge</h3>
+        <p>
+          Expect more. World class performance, with more privacy, more
+          productivity, and more value.
+        </p>
+        <a href="#">LEARN MORE</a>
+      </div>
+    </div>
+
+    <section class="xbox">
+      <div class="xbox-content">
+        <h2>Xbox Game Pass Ultimate</h2>
+        <p>
+          Xbox Game Pass Ultimate Xbox Live Gold and over 100 high-quality
+          console and PC games. Play together with friends and discover your
+          next favorite game.
+        </p>
+        <button><a href="#">Join Now</a></button>
+      </div>
+    </section>
+
+    <section class="card-container">
+      <div class="card">
+        <img src="images/card5.png" alt="" />
+        <h3>Microsoft Teams</h3>
+        <p>Unleash the power of your team</p>
+        <button class="btn-card"><a href="#">Shop Now</a></button>
+      </div>
+
+      <div class="card">
+        <img src="images/card6.jpg" alt="" />
+        <h3>Unlock the power learning</h3>
+        <p>
+          Get students future-ready with Windows 10 devices. Starting at $219
+        </p>
+        <button class="btn-card"><a href="#">Shop Now</a></button>
+      </div>
+
+      <div class="card">
+        <img src="images/card7.png" alt="" />
+        <h3>Windows 10 Enterprice</h3>
+        <p>Download the free 90-day evaluation for IT professionals.</p>
+        <button class="btn-card"><a href="#">Download Now</a></button>
+      </div>
+
+      <div class="card">
+        <img src="images/card8.png" alt="" />
+        <h3>Explore Kubernetes</h3>
+        <p>
+          Learn how Kubernetes works and get started with cloud native app
+          development today.
+        </p>
+        <button class="btn-card"><a href="#">Get Started</a></button>
+      </div>
+    </section>
+
+    <section class="natur">
+     
+        <h2>Commiting To Carbon Negative</h2>
+        <p>
+          Microsoft will be carbon negative by 2030 and by 2050 we will remove
+          all carbon the company has emitted since it was founded in 1975
+        </p>
+        <button><a href="#">Learn more</a></button>
+      
+    </section>
+
+    <section class="sozial">
+      <p>Follow Microsoft</p>
+      <img src="images/social-fb.png" alt="" />
+      <img src="images/social-linkedin.png" alt="" />
+      <img src="images/social-twitter.png" alt="" />
+    </section>
+
+    <nav class="links">
+      <ul>
+        <p>What's New</p>
+        <li><a href="#">Surface Pro X</a></li>
+        <li><a href="#">Surface Laptop</a></li>
+        <li><a href="#">Surface Pro 7</a></li>
+        <li><a href="#">Windows 10 apps</a></li>
+        <li><a href="#">Office apps</a></li>
+      </ul>
+
+      <ul>
+        <p>Microsoft Store</p>
+        <li><a href="#">Account Profile</a></li>
+        <li><a href="#">Download Center</a></li>
+        <li><a href="#">Microsoft Store Support</a></li>
+        <li><a href="#">Retums</a></li>
+        <li><a href="#">Older tracking </a></li>
+      </ul>
+
+      <ul>
+        <p>Educatio</p>
+        <li><a href="#">Microsoft in education</a></li>
+        <li><a href="#">Office for students</a></li>
+        <li><a href="#">Office 365 for schools</a></li>
+        <li><a href="#">Deals for students</a></li>
+        <li><a href="#">Microsoft Azure</a></li>
+      </ul>
+
+      <ul>
+        <p>Enterprice</p>
+        <li><a href="#">Azure </a></li>
+        <li><a href="#">App Source</a></li>
+        <li><a href="#">Automotive</a></li>
+        <li><a href="#">Govemment</a></li>
+        <li><a href="#">Healthcare</a></li>
+      </ul>
+
+      <ul>
+        <p>Developer</p>
+        <li><a href="#">Visual Studio</a></li>
+        <li><a href="#">Windows Dev Center</a></li>
+        <li><a href="#">Developer Network</a></li>
+        <li><a href="#">Tech Net</a></li>
+        <li><a href="#">Microsoft Developer</a></li>
+      </ul>
+
+      <ul>
+        <p>Company</p>
+        <li><a href="#">Careers </a></li>
+        <li><a href="#">About Microsoft</a></li>
+        <li><a href="#">Company news</a></li>
+        <li><a href="#">privacy at Microsoft</a></li>
+        <li><a href="#">Inverstore</a></li>
+      </ul>
+    </nav>
+
 </div>
 `;
 }
